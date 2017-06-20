ce88a41a0f8827cbffc3d84c09d6426b024c9a91
michaelveloso <calabiyau@gmail.com>
2017-06-19 21:32:10 -0400

    Handmade Quake 1.3 - strcmp and atoi

--- sys_win.c
+++ sys_win.c
@@ -1,21 +1,29 @@
 #include <Windows.h>
 #include <stdbool.h>
 #include <string.h>
+#include <stdint.h>
+
+typedef uint8_t uint8;
+typedef int32_t int32;
+typedef uint32_t uint32;
 
 #define MAX_NUM_ARGVS 50
 
-int argc = 1;
-char *largv[MAX_NUM_ARGVS];
+int32 argc = 1;
+uint8 *largv[MAX_NUM_ARGVS + 1];
 
-boolean IsPrintable(char character);
-boolean ArgumentsHasRoom(int numberOfArguments);
-int COM_CheckParm(char *parm);
+boolean IsPrintable(uint8 character);
+boolean ArgumentsHasRoom();
+boolean IsADigit(uint8 character);
+int32 COM_CheckParm(uint8 *parm);
+int32 Q_strcmp();
+int32 Q_atoi(uint8 *str);
 
-int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+int32 CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int32 nCmdShow)
 {
 	largv[0] = "";
 
-	while (*lpCmdLine && ArgumentsHasRoom(argc))
+	while (*lpCmdLine && ArgumentsHasRoom())
 	{
 		while (*lpCmdLine && !IsPrintable(*lpCmdLine))
 		{
@@ -39,29 +47,104 @@ int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
 			lpCmdLine++;
 		}
 	}
-
-	largv[argc] = "";
+	
+	int32 test = COM_CheckParm("-setalpha");
+	int32 value = Q_atoi(largv[test + 1]);
 	return 0;
 }
 
-boolean ArgumentsHasRoom(int numberOfArguments)
+boolean ArgumentsHasRoom()
 {
-	return (numberOfArguments < MAX_NUM_ARGVS);
+	return (argc < (MAX_NUM_ARGVS + 1));
 }
 
-boolean IsPrintable(char character)
+boolean IsPrintable(uint8 character)
 {
 	return ((character >= 33) && (character <= 126));
 }
 
-int COM_CheckParm(char *parm)
+int32 COM_CheckParm(uint8 *parm)
 {
-	for (int i = 1; i < argc; i++)
+	for (int32 i = 1; i < argc; i++)
 	{
-		if (!strcmp(parm, largv[i]))
+		if (!Q_strcmp(parm, largv[i]))
 		{
 			return i;
 		}
 	}
 	return 0;
+}
+
+int32 Q_strcmp(uint8 *s1, uint8 *s2)
+{
+	while (*s1 == *s2)
+	{
+		if (!*s1)
+		{
+			return 0;
+		}
+		++s1;
+		++s2;
+	}
+	
+	return (*s1 < *s2) ? -1 : 1;
+}
+
+int32 Q_atoi(uint8 *str)
+{
+	int32 sign = 1;
+	int32 val = 0;
+	uint8 character;
+
+	if (*str == '-')
+	{
+		sign = -1;
+		str++;
+	}
+
+	// hexadecimal
+	if (*str == '0' && (
+		str[1] == 'x' || str[1] == 'X')
+		)
+	{
+		str += 2;
+		while (true)
+		{
+			character = *str;
+			str++;
+			if (IsADigit(character))
+			{
+				val = val * 16 + (character - '0');
+			}
+			else if (character >= 'a' && character <= 'f')
+			{
+				val = val * 16 + (character - 'a' + 10);
+			}
+			else if (character >= 'A' && character <= 'F')
+			{
+				val = val * 16 + (character - 'a' + 10);
+			}
+			else
+			{
+				return val * sign;
+			}
+		}
+	}
+
+	// decimal
+	while (true)
+	{
+		character = *str;
+		str++;
+		if (!IsADigit(character))
+		{
+			return sign * val;
+		}
+		val = val * 10 + (character - '0');
+	}
+}
+
+boolean IsADigit(uint8 character)
+{
+	return (character >= '0' && character <= '9');
 }
